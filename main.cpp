49151f1749e4607916f9bc7f0002aa781f13981d
mihai-tuinea <mihai.tuinea27@gmail.com>
2024-10-29 17:53:14 +0200

    rezolvare stl 1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+struct Problema
+{
+    string IdProblema;
+    string Specializare;
+};
+
+struct Doctor
+{
+    string IdDoctor;
+    string Specializare;
+};
 
 int main()
 {
     ifstream inFile("input.txt");
 
-    int no_problems, no_doctors;
-    string name, speciality;
-    
-    inFile >> no_problems;
-
-    for (int i = 0; i < no_problems; i++)
+    int nr_probleme;
+    inFile >> nr_probleme;
+    vector<Problema> probleme(nr_probleme);
+    for (int i = 0; i < nr_probleme; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> probleme[i].IdProblema >> probleme[i].Specializare;
     }
 
-    inFile >> no_doctors;
-
-    for (int i = 0; i < no_doctors; i++)
+    int nr_doctori;
+    inFile >> nr_doctori;
+    vector<Doctor> doctori(nr_doctori);
+    for (int i = 0; i < nr_doctori; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> doctori[i].IdDoctor >> doctori[i].Specializare;
     }
 
+    for (const auto& p : probleme)
+    {
+        auto it = find_if(begin(doctori), end(doctori), [&p](const Doctor& d) {return p.Specializare == d.Specializare; });
+        if (it != end(doctori))
+            cout << p.IdProblema << " Acceptat\n";
+        else
+            cout << p.IdProblema << " Respins\n";
+    }
     return 0;
 }
